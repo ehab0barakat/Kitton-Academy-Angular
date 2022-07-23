9bf969b88dd2b1029b13d9a8039e18e056304dd6
Rahma.Mahmoud.Ahmed <rahmamahmoud2222@gmail.com>
2022-07-23 14:41:21 +0200

    error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:MainLayoutComponent,children:[
+
+  ]},
+  {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
