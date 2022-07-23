f96b096bc8ecfc78fcef3099ea6046b06ca1369a
ehab0barakat <mrehab9797@gmail.com>
2022-07-23 08:07:04 +0200

    ehab good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GamesComponent } from './Components/Games/games/games.component';
+import { SingleGameComponent } from './Components/Games/single-game/single-game.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "home" , component: HomeComponent},
+  {path : "games-archive" , component: GamesComponent},
+  {path : "single-game" , component: SingleGameComponent},  // gonna be changed >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { GamesComponent } from './Components/Games/games/games.component';
+import { SingleGameComponent } from './Components/Games/single-game/single-game.component';
+import { EventsArchiveComponent } from './Components/Events/events-archive/events-archive.component';
+import { EventsSingleComponent } from './Components/Events/events-single/events-single.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CrudGameComponent } from './Components/Games/crud-game/crud-game.component';
+import { CrudEventComponent } from './Components/Events/crud-event/crud-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    GamesComponent,
+    SingleGameComponent,
+    EventsArchiveComponent,
+    EventsSingleComponent,
+    HomeComponent,
+    CrudGameComponent,
+    CrudEventComponent
   ],
   imports: [
     BrowserModule,
