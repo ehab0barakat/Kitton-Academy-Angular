07dbac7d4883a793ba2055fc21dc937713b3cfdd
ehab0barakat <mrehab9797@gmail.com>
2022-07-23 08:02:57 +0200

    event && game && home

---
+++ src/app/Components/Events/events-archive/events-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-archive',
+  templateUrl: './events-archive.component.html',
+  styleUrls: ['./events-archive.component.css']
+})
+export class EventsArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Components/Events/events-single/events-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-single',
+  templateUrl: './events-single.component.html',
+  styleUrls: ['./events-single.component.css']
+})
+export class EventsSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
